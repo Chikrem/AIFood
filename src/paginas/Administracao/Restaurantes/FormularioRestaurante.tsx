507a72943748cf0e4e32ee175794a6d269f5378c
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-03-11 19:14:11 -0300

    Extraindo o layout base para nova página

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,68 +1,59 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import http from "../../../http";
+import { Box, Button, TextField, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import http from "../../../http"
+import IRestaurante from "../../../interfaces/IRestaurante"
 
 const FormularioRestaurante = () => {
-  const parametros = useParams();
 
-  useEffect(() => {
-    if (parametros.id) {
-      http
-        .get(`restaurantes/${parametros.id}/`)
-        .then((resposta) => {
-          setNomeRestaurante(resposta.data.nome);
-        });
+    const parametros = useParams()
+
+    useEffect(() => {
+        if (parametros.id) {
+            http.get<IRestaurante>(`restaurantes/${parametros.id}/`)
+                .then(resposta => setNomeRestaurante(resposta.data.nome))
+        }
+    }, [parametros])
+
+    const [nomeRestaurante, setNomeRestaurante] = useState('')
+
+    const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+
+        if (parametros.id) {
+            http.put(`restaurantes/${parametros.id}/`, {
+                nome: nomeRestaurante
+            })
+                .then(() => {
+                    alert("Restaurante atualizado com sucesso!")
+                })
+        } else {
+            http.post('restaurantes/', {
+                nome: nomeRestaurante
+            })
+                .then(() => {
+                    alert("Restaurante cadastrado com sucesso!")
+                })
+        }
+
     }
-  }, [parametros]);
 
-  const [nomeRestaurante, setNomeRestaurante] = useState<string>("");
+    return (
+        <Box sx={{ display: 'flex', flexDirection: "column", alignItems: "center", flexGrow: 1 }}>
+            <Typography component="h1" variant="h6">Formulário de Restaurantes</Typography>
+            <Box component="form" sx={{ width: '100%' }} onSubmit={aoSubmeterForm}>
+                <TextField
+                    value={nomeRestaurante}
+                    onChange={evento => setNomeRestaurante(evento.target.value)}
+                    label="Nome do Restaurante"
+                    variant="standard"
+                    fullWidth
+                    required
+                />
+                <Button sx={{ marginTop: 1 }} type="submit" fullWidth variant="outlined">Salvar</Button>
+            </Box>
+        </Box>
+    )
+}
 
-  const aoSubmeter = (evento: React.FormEvent<HTMLFormElement>) => {
-    evento.preventDefault();
-    if (parametros.id) {
-      // Atualiza restaurante existente
-      http
-        .put(`restaurantes/${parametros.id}/`, {
-          nome: nomeRestaurante,
-        })
-        .then(() => {
-          alert("Restaurante atualizado com sucesso");
-        });
-    } else {
-      // Cria novo restaurante
-      http
-        .post("restaurantes/", {
-          nome: nomeRestaurante,
-        })
-        .then(() => {
-          alert("Restaurante cadastrado com sucesso");
-        });
-    }
-  };
-
-  return (
-    <Box sx={{ display: "flex", marginTop: 5, flexDirection: "column", gap: 2, alignItems: "center" }}>
-      <Box component="form" onSubmit={aoSubmeter}>
-        <Typography component="h1" variant="h4">
-          Formulário de Restaurantes
-        </Typography>
-        <TextField
-          value={nomeRestaurante}
-          onChange={(evento) => setNomeRestaurante(evento.target.value)}
-          required
-          id="standard-input"
-          label="Nome"
-          type="standard"
-          variant="standard"
-          fullWidth
-        />
-        <Button sx={{ marginTop: 1 }} fullWidth type="submit" variant="outlined">
-          Salvar
-        </Button>
-      </Box>
-    </Box>
-  );
-};
-
-export default FormularioRestaurante;
+export default FormularioRestaurante
